ddb0d8b14f77dcef96f2b2188d7de598cd8aa40a
imskanand <the.anand3099@gmail.com>
2022-12-30 19:21:05 +0530

    Basic Validation in Zod

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ const student = z.object({
 	userName: z.string().min(5),
 	age: z.number().gt(0),
 	birthday: z.date(),
-	isProgrammer: z.boolean().optional(),
+	isProgrammer: z.boolean().default(false),
+	gender: z.enum(['Male', 'Female', 'Other']),
 });
 
-// type Student = z.infer<typeof student>;
+type Student = z.infer<typeof student>;
 
 const student1 = {
 	userName: 'imskanand',
 	age: 22,
 	birthday: new Date(),
 	isProgrammer: true,
+	gender: 'Male',
 };
 
-console.log(student.safeParse(student1).success);
+console.log(student.safeParse(student1));

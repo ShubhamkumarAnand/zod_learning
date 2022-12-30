e3bec387ea3ec86f3adf24c1c3af8fd5d1ea682d
imskanand <the.anand3099@gmail.com>
2022-12-30 19:31:40 +0530

    Object validation and methods

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import { z } from 'zod';
 
-const student = z.object({
-	userName: z.string().min(5),
-	age: z.number().gt(0),
-	birthday: z.date(),
-	isProgrammer: z.boolean().default(false),
-	gender: z.enum(['Male', 'Female', 'Other']),
-});
+const studentSchema = z
+	.object({
+		userName: z.string().min(5),
+		age: z.number().gt(0),
+		birthday: z.date(),
+		isProgrammer: z.boolean().default(false),
+		gender: z.enum(['Male', 'Female', 'Other']),
+	})
+	.extend({ isDoingImpressive: z.boolean().default(true) })
+	.passthrough();
 
-type Student = z.infer<typeof student>;
+// partial() makes Everything Optional
+// passthrough() makes everything pass through the additional fields
+type Student = z.infer<typeof studentSchema>;
 
 const student1 = {
 	userName: 'imskanand',
@@ -18,4 +23,8 @@ const student1 = {
 	gender: 'Male',
 };
 
-console.log(student.safeParse(student1));
+console.log(studentSchema.safeParse(student1).success);
+console.log(studentSchema.safeParse(student1));
+
+// object type
+console.log(studentSchema.shape);
